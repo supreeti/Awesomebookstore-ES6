6461152dfafcff43922c68296f2d229609b4a68f
Supreeti <sksupreetk11@gmail.com>
2023-05-05 00:20:14 +0530

    resolve linter errors

--- modules/Navigation.js
+++ modules/Navigation.js
@@ -2,19 +2,19 @@ const navbar = document.querySelectorAll('.links li');
 
 navbar.forEach((link) => {
     link.addEventListener('click', () => {
-      if (link.innerText === 'List') {
-        document.querySelector('.book').classList.remove('hide');
-        document.querySelector('.addbook').classList.add('hide');
-        document.querySelector('.contat').classList.add('hide');
-      } else if (link.innerText === 'Add') {
-        document.querySelector('.book').classList.add('hide');
-        document.querySelector('.addbook').classList.remove('hide');
-        document.querySelector('.contat').classList.add('hide');
-      } else if (link.innerText === 'Contact') {
-        document.querySelector('.book').classList.add('hide');
-        document.querySelector('.addbook').classList.add('hide');
-        document.querySelector('.contat').classList.remove('hide');
-      }
+        if (link.innerText === 'List') {
+            document.querySelector('.book').classList.remove('hide');
+            document.querySelector('.addbook').classList.add('hide');
+            document.querySelector('.contat').classList.add('hide');
+        } else if (link.innerText === 'Add') {
+            document.querySelector('.book').classList.add('hide');
+            document.querySelector('.addbook').classList.remove('hide');
+            document.querySelector('.contat').classList.add('hide');
+        } else if (link.innerText === 'Contact') {
+            document.querySelector('.book').classList.add('hide');
+            document.querySelector('.addbook').classList.add('hide');
+            document.querySelector('.contat').classList.remove('hide');
+        }
     });
-  });
-  export default navbar;
+});
+export default navbar;
